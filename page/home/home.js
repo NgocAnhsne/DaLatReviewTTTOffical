27b8b6829d76d3764bbaa2bd783deb774b0cbcec
ngocanhsne <ngocanhne2402@gmail.com>
2024-01-25 16:35:58 +0700

    first

---
+++ page/home/home.js
@@ -0,0 +1,36 @@
+const ListImage = document.querySelector('.home__slider--list__images');
+const imgs = document.querySelectorAll('.home__slider--item__image');
+const btnLeft = document.querySelector('.home__slider--btn__left');
+const btnRight = document.querySelector('.home__slider--btn__right');
+const lenght = imgs.length;
+let current = 0;
+
+const handleChangeSlide = () =>{
+    if(current == lenght - 1){
+        current = 0;
+        let width = imgs[0].offsetWidth;
+        ListImage.style.transform = `translateX(0px)`;
+    }else{
+        current ++;
+        let width = imgs[0].offsetWidth;
+        ListImage.style.transform = `translateX(${width * -1 * current}px)`;
+    }
+}
+
+setInterval(handleChangeSlide,4000);
+
+btnRight.addEventListener('click',()=>{
+    handleChangeSlide();
+})
+
+btnLeft.addEventListener('click',()=>{
+    if(current == 0){
+        current = lenght - 1;
+        let width = imgs[0].offsetWidth;
+        ListImage.style.transform = `translateX(${width * -1 * current}px)`;
+    }else{
+        current --;
+        let width = imgs[0].offsetWidth;
+        ListImage.style.transform = `translateX(${width * -1 * current}px)`;
+    }
+})
